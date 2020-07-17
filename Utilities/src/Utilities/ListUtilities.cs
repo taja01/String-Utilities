08df6e9f63e43a112f4d8de788822167be9f0007
taja01 <tamas.jarvas@betssongroup.com>
2020-07-17 13:09:13 +0200

    add IsInAlphabeticalOrder

---
+++ Utilities/src/Utilities/ListUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Utilities
+{
+    public class ListUtilities
+    {
+        //https://docs.microsoft.com/en-us/globalization/locale/sorting-and-string-comparison
+        public static bool IsInAlphabeticalOrder(IList<string> list)
+        {
+            var current = list.First();
+            foreach (var item in list.Skip(1))
+            {
+                var next = item;
+                var sc1 = CultureInfo.DefaultThreadCurrentCulture.CompareInfo.GetSortKey(current);
+                var sc2 = CultureInfo.DefaultThreadCurrentCulture.CompareInfo.GetSortKey(next);
+                var result = SortKey.Compare(sc1, sc2);
+
+                // Assert.LessOrEqual(result, 0, $"Alphabetical order problem: {next} is before the {current}");
+                if (result > 0)
+                {
+                    return false;
+                }
+
+                current = next;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Utilities/test/UtilitiesTest/ListTests/ListUtilitiesTests_EN.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Globalization;
+using Utilities;
+
+namespace UtilitiesTest
+{
+    [TestFixture]
+    public class ListUtilitiesTests_EN
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+        }
+
+        [Test]
+        public void ListTest1()
+        {
+            var listA = new List<string> { "0", "1", "2", "3" };
+            Assert.That(ListUtilities.IsInAlphabeticalOrder(listA), Is.True);
+        }
+
+        [Test]
+        public void ListTest2()
+        {
+            var listA = new List<string> { "a0", "a1", "a2", "a3" };
+            Assert.That(ListUtilities.IsInAlphabeticalOrder(listA), Is.True);
+        }
+
+        [Test]
+        public void ListTest3()
+        {
+            var listA = new List<string> { "a0", "a0", "a0", "a0" };
+            Assert.That(ListUtilities.IsInAlphabeticalOrder(listA), Is.True);
+        }
+
+        [Test]
+        public void ListTest4()
+        {
+            var listA = new List<string> { "r", "s", "t", "g" };
+            Assert.That(ListUtilities.IsInAlphabeticalOrder(listA), Is.False);
+        }
+    }
+}
